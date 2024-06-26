753e636ed24a8f8ed3d715b4ba6b76f2ac8c86c1
Yazan <yazansamer123qwe@gmail.com>
2024-06-26 18:41:58 +0300

    3mol lashof

--- backend/controllers/compnayController.js
+++ backend/controllers/compnayController.js
@@ -4,45 +4,41 @@ const Contract = require('../models/contractModel')
 const addUserToCompnay = async (req,res) => {
     try {
         const { email , role } = req.body
-        const { companyId , userId } = req.params
+        const { companyId  } = req.params
 
         const company = await Company.findById(companyId)
-        const user = await Users.findById(userId)
-        const contract = await Contract.findOne({companyId, userId})
-        if(contract){
-            return res.status(400).json({ message: 'User is already in this company'});
-        }
-        
+        const user = await Users.findOne({email})
         if(!company){
             return res.status(404).json({msg : 'Company not found'})
         }
-
+        
         if(!user){
             return res.status(404).json({msg : 'User not found'})
         }
 
-        if (user.email !== email) {
-            return res.status(400).json({ message: 'Email does not match user' });
+        const contract = await Contract.findOne({companyId,userId:user._id})
+        if(contract){
+            return res.status(400).json({ message: 'User is already in this company'});
         }
-
-        if (company.admins.includes(userId)) {
+        
+        if (company.admins.includes(user._id)) {
             return res.status(400).json({ message: 'User is already an admin of this company'});
         }
 
         if(role == "admin"){
-            company.admins.push(userId);
+            company.admins.push(user._id);
             await company.save()
-            res.status(200).json({ message : "Add to company successfully"})
-        }else {
-            const newContract = new Contract({
-                companyId,
-                userId,
-                role,
-                joinDate : new Date()
-            })
-            await newContract.save();
-            res.status(200).json({ message : "Add to contract successfully"})
+            return res.status(200).json({ message : "Add to company successfully"})
         }
+        const newContract = new Contract({
+            companyId,
+            userId:user._id,
+            role,
+            joinDate : new Date()
+        })
+        await newContract.save();
+        res.status(200).json({ message : "Add to contract successfully"})
+        
 
     } catch (error) {
         res.status(500).json({message: error.message});
