4554c7a48425256c33980fb3609893780065e0e4
TareqSAA <alkhateb31999@gmail.com>
2024-06-30 01:49:41 +0300

    tareq

---
+++ backend/routers/traineeRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const traineeController = require('../controllers/traineeController')
+
+router.get('/' , traineeController.getCompanies)
+router.get('/:companyId/courses'  , traineeController.getCompanyCources)
+router.get('/courses/:courseId'  , traineeController.getcourseinfo)
+router.get('/courses/:courseId/lessons/:lessonId',traineeController.getlesson)
+router.get('/courses/:courseId/lessons/:lessonId/quizSolution',traineeController.getQuizSolution)
+module.exports = router
