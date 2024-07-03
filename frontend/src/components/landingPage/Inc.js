950959cdca4d524182080a5bf8c0d1831f648f1b
rm490 <rema.dahhak@gmail.com>
2024-07-03 08:48:52 +0400

    landingPage

---
+++ frontend/src/components/landingPage/Inc.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./inc.css'
+
+function Inc() {
+  return (
+    <div className='inc'>
+      <b >2024 OwlNest Inc. All rights reserved.</b>
+    </div>
+  )
+}
+
+export default Inc
